63b9b217a2976230b20972257cc8b21da9d75c1b
samuoquendo25 <samueloquendo030@gmail.com>
2026-03-04 20:24:11 -0500

    se agrega metodo root para evitar conflictos

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+import json
 
 app = FastAPI()
 
-#creamos la clase Reserva como modelo
+ARCHIVO_JSON = 'reservas.json'
+
 class Reserva(BaseModel):
     id_reserva: Optional[int] = None
     id_sala: Optional[int] = None
@@ -15,4 +17,33 @@ class Reserva(BaseModel):
     personas: Optional[int] = None
     estado: Optional[bool] = None
 
-reservas_db = []
+def leer_datos():
+    try:
+        with open(ARCHIVO_JSON, 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+def guardar_datos(datos):
+    with open(ARCHIVO_JSON, 'w') as file:
+        json.dump(datos, file, indent=4)
+
+@app.post("/reservas")
+async def reservar_sala(reserva: Reserva):
+    lista_datos = leer_datos()
+    
+    nueva_reserva = reserva.model_dump()
+    
+    lista_datos.append(nueva_reserva)
+    
+    guardar_datos(lista_datos)
+    
+    return {"message": "Reserva guardada con éxito", "data": nueva_reserva}
+
+@app.get("/reservas/listar")
+async def obtener_reservas():
+    return leer_datos()
+
+@app.get("/")
+def root():
+    return {"mensaje": "API funcionando"}
